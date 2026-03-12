bcf8eb7be93fc6c086a23ac667b9eccb32e30e89
[$] Mr 574rk [$] <66188165+mr-574rk@users.noreply.github.com>
2026-03-12 13:12:08 +0100

    v1

---
+++ modules/metadata.py
@@ -0,0 +1,3 @@
+name = 'DAPv3'
+version = '2.0.3c'
+edition = 'GitHub Edition'
---
+++ modules/paths.py
@@ -0,0 +1,6 @@
+"""Shared path constants for the DAPv3 project."""
+
+import os
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODELS_DIR = os.path.join(ROOT_DIR, "models")
